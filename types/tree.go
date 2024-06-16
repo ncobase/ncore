339f173b9e75d8b7efb7d632ee8422279b8903a6
Shen <ihaiyon@gmail.com>
2024-06-16 17:18:31 +0800

    build tree nodes

---
+++ types/tree.go
@@ -0,0 +1,39 @@
+package types
+
+import "ncobase/common/validator"
+
+// TreeNode represents a tree node.
+type TreeNode interface {
+	GetID() string
+	GetParentID() string
+	SetChildren([]TreeNode)
+	GetChildren() []TreeNode
+}
+
+// BuildTree builds a tree structure from the given nodes.
+func BuildTree[T TreeNode](nodes []T) []T {
+	nodeMap := make(map[string]T, len(nodes))
+	for _, node := range nodes {
+		nodeMap[node.GetID()] = node
+	}
+
+	treeMap := make(map[string]T, len(nodes))
+	for _, node := range nodeMap {
+		if validator.IsEmpty(node.GetParentID()) {
+			treeMap[node.GetID()] = node
+		} else {
+			if parent, ok := nodeMap[node.GetParentID()]; ok {
+				parent.SetChildren(append(parent.GetChildren(), node))
+			} else {
+				treeMap[node.GetID()] = node
+			}
+		}
+	}
+
+	result := make([]T, 0, len(treeMap))
+	for _, root := range treeMap {
+		result = append(result, root)
+	}
+
+	return result
+}
