e1578caad641de5451651ea3f3e4ad1061b815e2
Shen <ihaiyon@gmail.com>
2024-06-29 21:32:32 +0800

    plugin support hot reload * adjust config * add manager routes

--- config/plugin.go
+++ config/plugin.go
@@ -4,16 +4,18 @@ import "github.com/spf13/viper"
 
 // Plugin plugin config struct
 type Plugin struct {
-	Path     string
-	Includes []string
-	Excludes []string
+	Path      string
+	Includes  []string
+	Excludes  []string
+	HotReload bool
 }
 
 // getPluginConfig returns the plugin config
 func getPluginConfig(v *viper.Viper) Plugin {
 	return Plugin{
-		Path:     v.GetString("plugin.path"),
-		Includes: v.GetStringSlice("plugin.includes"),
-		Excludes: v.GetStringSlice("plugin.excludes"),
+		Path:      v.GetString("plugin.path"),
+		Includes:  v.GetStringSlice("plugin.includes"),
+		Excludes:  v.GetStringSlice("plugin.excludes"),
+		HotReload: v.GetBool("plugin.hot_reload"),
 	}
 }
