f8d23fc3e25361994ceb858953aec69b98580a3b
Shen <ihaiyon@gmail.com>
2024-08-31 11:28:40 +0800

    primary key use base62

--- consts/char.go
+++ consts/char.go
@@ -2,16 +2,16 @@ package consts
 
 // Character sets
 const (
-	Number        = "0123456789"                            // Numbers
-	Lowercase     = "abcdefghijklmnopqrstuvwxyz"            // Lowercase letters
-	Uppercase     = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"            // Uppercase letters
-	Symbol        = "!#$%&()*+,-./:;<=>?@[]^_`{|}~"         // Symbols
-	NumLower      = Number + Lowercase                      // Numbers + Lowercase letters
-	NumUpper      = Number + Uppercase                      // Numbers + Uppercase letters
-	LowerUpper    = Lowercase + Uppercase                   // Lowercase + Uppercase letters
-	NumLowerUpper = Number + Lowercase + Uppercase          // Numbers + Lowercase + Uppercase letters
-	All           = Number + Lowercase + Uppercase + Symbol // Combination of all
+	Number        = "0123456789"                    // Numbers
+	Lowercase     = "abcdefghijklmnopqrstuvwxyz"    // Lowercase letters
+	Uppercase     = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"    // Uppercase letters
+	Symbol        = "!#$%&()*+,-./:;<=>?@[]^_`{|}~" // Symbols
+	NumLower      = Number + Lowercase              // Numbers + Lowercase letters
+	NumUpper      = Number + Uppercase              // Numbers + Uppercase letters
+	LowerUpper    = Lowercase + Uppercase           // Lowercase + Uppercase letters
+	NumLowerUpper = Number + Lowercase + Uppercase  // Numbers + Lowercase + Uppercase letters
+	All           = NumLowerUpper + Symbol          // Combination of all
 )
 
 // PrimaryKey is the character set used for primary keys
-const PrimaryKey = Number + Lowercase + Uppercase + "_-"
+const PrimaryKey = NumLowerUpper
