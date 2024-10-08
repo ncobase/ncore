b27c0374776c1741ee155d19c9825fee9dff528a
Shen <ihaiyon@gmail.com>
2024-10-08 17:45:24 +0800

    improved log and logger

--- log/log.go
+++ log/log.go
@@ -26,127 +26,132 @@ const (
 	SpanFunctionKey = "function"
 )
 
+type Logger struct {
+	*logrus.Logger
+	version     string
+	logFile     *os.File
+	logPath     string
+	meiliClient *meili.Client
+	esClient    *elastic.Client
+	indexName   string // Meilisearch / Elasticsearch index name
+}
+
 var (
-	standardLogger *logrus.Logger
+	standardLogger *Logger
 	once           sync.Once
-	version        string
-	logFile        *os.File
-	logPath        string
-	meiliClient    *meili.Client
-	esClient       *elastic.Client
-	indexName      string // Meilisearch / Elasticsearch index name
 )
 
 // StandardLogger returns the singleton logger instance
-func StandardLogger() *logrus.Logger {
+func StandardLogger() *Logger {
 	once.Do(func() {
-		standardLogger = logrus.New()
+		standardLogger = &Logger{
+			Logger: logrus.New(),
+		}
 		standardLogger.SetFormatter(&logrus.JSONFormatter{})
 	})
 	return standardLogger
 }
 
 // SetVersion sets the version for logging
-func SetVersion(v string) {
-	version = v
+func (l *Logger) SetVersion(v string) {
+	l.version = v
 }
 
 // Init initializes the logger with the given configuration
-func Init(c *config.Logger) (func(), error) {
-	logger := StandardLogger()
-	logger.SetLevel(logrus.Level(c.Level))
+func (l *Logger) Init(c *config.Logger) (func(), error) {
+	l.SetLevel(logrus.Level(c.Level))
 
 	switch c.Format {
 	case "json":
-		logger.SetFormatter(&logrus.JSONFormatter{})
+		l.SetFormatter(&logrus.JSONFormatter{})
 	default:
-		logger.SetFormatter(&logrus.TextFormatter{})
+		l.SetFormatter(&logrus.TextFormatter{})
 	}
 
 	switch c.Output {
 	case "stdout":
-		logger.SetOutput(os.Stdout)
+		l.SetOutput(os.Stdout)
 	case "stderr":
-		logger.SetOutput(os.Stderr)
+		l.SetOutput(os.Stderr)
 	case "file":
-		logPath = c.OutputFile
-		if logPath != "" {
-			if err := setupLogFile(); err != nil {
+		l.logPath = c.OutputFile
+		if l.logPath != "" {
+			if err := l.setupLogFile(); err != nil {
 				return nil, err
 			}
-			go periodicLogRotation()
+			go l.periodicLogRotation()
 		}
 	}
 
 	// Initialize MeiliSearch client
 	if c.Meilisearch.Host != "" {
-		meiliClient = meili.NewMeilisearch(c.Meilisearch.Host, c.Meilisearch.APIKey)
-		indexName = c.IndexName
-		AddMeiliSearchHook()
+		l.meiliClient = meili.NewMeilisearch(c.Meilisearch.Host, c.Meilisearch.APIKey)
+		l.indexName = c.IndexName
+		l.AddMeiliSearchHook()
 	}
 
 	// Initialize Elasticsearch client
 	if len(c.Elasticsearch.Addresses) > 0 {
 		var err error
-		esClient, err = elastic.NewClient(c.Elasticsearch.Addresses, c.Elasticsearch.Username, c.Elasticsearch.Password)
+		l.esClient, err = elastic.NewClient(c.Elasticsearch.Addresses, c.Elasticsearch.Username, c.Elasticsearch.Password)
 		if err != nil {
 			return nil, fmt.Errorf("error initializing Elasticsearch client: %w", err)
 		}
-		indexName = c.IndexName
-		AddElasticSearchHook()
+		l.indexName = c.IndexName
+		l.AddElasticSearchHook()
 	}
 
 	// Return cleanup function
 	return func() {
-		if logFile != nil {
-			_ = logFile.Close()
+		if l.logFile != nil {
+			_ = l.logFile.Close()
 		}
 	}, nil
 }
 
-func setupLogFile() error {
-	if err := os.MkdirAll(filepath.Dir(logPath), 0777); err != nil {
+func (l *Logger) setupLogFile() error {
+	if err := os.MkdirAll(filepath.Dir(l.logPath), 0777); err != nil {
 		return err
 	}
-	return rotateLog()
+	return l.rotateLog()
 }
 
-func rotateLog() error {
-	if logFile != nil {
-		if err := logFile.Close(); err != nil {
+func (l *Logger) rotateLog() error {
+	if l.logFile != nil {
+		if err := l.logFile.Close(); err != nil {
 			return err
 		}
 	}
 
-	logFilePath := fmt.Sprintf("%s.%s.log", strings.TrimSuffix(logPath, ".log"), time.Now().Format("2006-01-02"))
+	logFilePath := fmt.Sprintf("%s.%s.log", strings.TrimSuffix(l.logPath, ".log"), time.Now().Format("2006-01-02"))
 	f, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return err
 	}
 
-	logFile = f
-	StandardLogger().SetOutput(logFile)
+	l.logFile = f
+	l.SetOutput(l.logFile)
 	return nil
 }
 
-func periodicLogRotation() {
+func (l *Logger) periodicLogRotation() {
 	ticker := time.NewTicker(24 * time.Hour)
 	defer ticker.Stop()
 
 	for range ticker.C {
-		if err := rotateLog(); err != nil {
-			StandardLogger().Errorf("Error rotating log: %v", err)
+		if err := l.rotateLog(); err != nil {
+			l.Logger.Errorf("Error rotating log: %v", err)
 		}
 	}
 }
 
 // EntryWithFields creates a new log entry with the given fields and context
-func EntryWithFields(ctx context.Context, fields logrus.Fields) *logrus.Entry {
-	return entryFromContext(ctx).WithFields(fields)
+func (l *Logger) EntryWithFields(ctx context.Context, fields logrus.Fields) *logrus.Entry {
+	return l.entryFromContext(ctx).WithFields(fields)
 }
 
 // entryFromContext creates a new log entry with fields from context
-func entryFromContext(ctx context.Context) *logrus.Entry {
+func (l *Logger) entryFromContext(ctx context.Context) *logrus.Entry {
 	fields := logrus.Fields{}
 
 	traceID := getTraceID(ctx)
@@ -155,116 +160,123 @@ func entryFromContext(ctx context.Context) *logrus.Entry {
 	}
 	fields[TraceIDKey] = traceID
 
-	if version != "" {
-		fields[VersionKey] = version
+	if l.version != "" {
+		fields[VersionKey] = l.version
 	}
 
-	return StandardLogger().WithFields(fields)
+	return l.WithFields(fields)
 }
 
-func Infof(ctx context.Context, format string, args ...any) {
-	entryFromContext(ctx).Infof(format, args...)
+// Log methods
+func (l *Logger) Infof(ctx context.Context, format string, args ...any) {
+	l.entryFromContext(ctx).Infof(format, args...)
 }
 
-func Debugf(ctx context.Context, format string, args ...any) {
-	entryFromContext(ctx).Debugf(format, args...)
+func (l *Logger) Debugf(ctx context.Context, format string, args ...any) {
+	l.entryFromContext(ctx).Debugf(format, args...)
 }
 
-func Warnf(ctx context.Context, format string, args ...any) {
-	entryFromContext(ctx).Warnf(format, args...)
+func (l *Logger) Warnf(ctx context.Context, format string, args ...any) {
+	l.entryFromContext(ctx).Warnf(format, args...)
 }
 
-func Errorf(ctx context.Context, format string, args ...any) {
-	entryFromContext(ctx).Errorf(format, args...)
+func (l *Logger) Errorf(ctx context.Context, format string, args ...any) {
+	l.entryFromContext(ctx).Errorf(format, args...)
 }
 
-func Fatalf(ctx context.Context, format string, args ...any) {
-	entryFromContext(ctx).Fatalf(format, args...)
+func (l *Logger) Fatalf(ctx context.Context, format string, args ...any) {
+	l.entryFromContext(ctx).Fatalf(format, args...)
 }
 
-func Panicf(ctx context.Context, format string, args ...any) {
-	entryFromContext(ctx).Panicf(format, args...)
+func (l *Logger) Panicf(ctx context.Context, format string, args ...any) {
+	l.entryFromContext(ctx).Panicf(format, args...)
 }
 
-func Info(ctx context.Context, args ...any) {
-	entryFromContext(ctx).Info(args...)
+func (l *Logger) Info(ctx context.Context, args ...any) {
+	l.entryFromContext(ctx).Info(args...)
 }
 
-func Debug(ctx context.Context, args ...any) {
-	entryFromContext(ctx).Debug(args...)
+func (l *Logger) Debug(ctx context.Context, args ...any) {
+	l.entryFromContext(ctx).Debug(args...)
 }
 
-func Warn(ctx context.Context, args ...any) {
-	entryFromContext(ctx).Warn(args...)
+func (l *Logger) Warn(ctx context.Context, args ...any) {
+	l.entryFromContext(ctx).Warn(args...)
 }
 
-func Error(ctx context.Context, args ...any) {
-	entryFromContext(ctx).Error(args...)
+func (l *Logger) Error(ctx context.Context, args ...any) {
+	l.entryFromContext(ctx).Error(args...)
 }
 
-func Fatal(ctx context.Context, args ...any) {
-	entryFromContext(ctx).Fatal(args...)
+func (l *Logger) Fatal(ctx context.Context, args ...any) {
+	l.entryFromContext(ctx).Fatal(args...)
 }
 
-func Panic(ctx context.Context, args ...any) {
-	entryFromContext(ctx).Panic(args...)
+func (l *Logger) Panic(ctx context.Context, args ...any) {
+	l.entryFromContext(ctx).Panic(args...)
 }
 
 // MeiliSearch and Elasticsearch log hooks
 
-type MeiliSearchHook struct{}
+type MeiliSearchHook struct {
+	client *meili.Client
+	index  string
+}
 
 func (h *MeiliSearchHook) Levels() []logrus.Level {
 	return logrus.AllLevels
 }
 
 func (h *MeiliSearchHook) Fire(entry *logrus.Entry) error {
-	if meiliClient == nil {
-		return nil
-	}
 	jsonData, err := json.Marshal(entry.Data)
 	if err != nil {
 		return err
 	}
-	return meiliClient.IndexDocuments(indexName, jsonData)
+	return h.client.IndexDocuments(h.index, jsonData)
 }
 
-type ElasticSearchHook struct{}
+type ElasticSearchHook struct {
+	client *elastic.Client
+	index  string
+}
 
 func (h *ElasticSearchHook) Levels() []logrus.Level {
 	return logrus.AllLevels
 }
 
 func (h *ElasticSearchHook) Fire(entry *logrus.Entry) error {
-	if esClient == nil {
-		return nil
-	}
-	return esClient.IndexDocument(context.Background(), indexName, entry.Time.Format(time.RFC3339), entry.Data)
+	return h.client.IndexDocument(context.Background(), h.index, entry.Time.Format(time.RFC3339), entry.Data)
 }
 
 // AddMeiliSearchHook adds MeiliSearch hook to logrus
-func AddMeiliSearchHook() {
-	if meiliClient != nil {
-		hook := &MeiliSearchHook{}
-		if !hookExists(hook) {
-			StandardLogger().AddHook(hook)
+func (l *Logger) AddMeiliSearchHook() {
+	if l.meiliClient != nil {
+		hook := &MeiliSearchHook{
+			client: l.meiliClient,
+			index:  l.indexName,
+		}
+		if !l.hookExists(hook) {
+			l.AddHook(hook)
 		}
 	}
 }
 
 // AddElasticSearchHook adds Elasticsearch hook to logrus
-func AddElasticSearchHook() {
-	if esClient != nil {
-		hook := &ElasticSearchHook{}
-		if !hookExists(hook) {
-			StandardLogger().AddHook(hook)
+func (l *Logger) AddElasticSearchHook() {
+	if l.esClient != nil {
+		hook := &ElasticSearchHook{
+			client: l.esClient,
+			index:  l.indexName,
+		}
+		if !l.hookExists(hook) {
+			l.AddHook(hook)
 		}
 	}
 }
 
 // hookExists checks if hook already exists
-func hookExists(hook logrus.Hook) bool {
-	for _, h := range StandardLogger().Hooks {
+func (l *Logger) hookExists(hook logrus.Hook) bool {
+	for _, h := range l.Hooks {
 		for _, existingHook := range h {
 			if existingHook == hook {
 				return true
@@ -275,11 +287,44 @@ func hookExists(hook logrus.Hook) bool {
 }
 
 // SetOutput sets the output destination for the logger
-func SetOutput(out io.Writer) {
-	StandardLogger().SetOutput(out)
+func (l *Logger) SetOutput(out io.Writer) {
+	l.Logger.SetOutput(out)
 }
 
-// AddHook adds a hook to the standard logger
-func AddHook(hook logrus.Hook) {
-	StandardLogger().AddHook(hook)
+// AddHook adds a hook to the logger
+func (l *Logger) AddHook(hook logrus.Hook) {
+	l.Logger.AddHook(hook)
 }
+
+// Exported functions for backward compatibility
+func SetVersion(v string)                   { StandardLogger().SetVersion(v) }
+func Init(c *config.Logger) (func(), error) { return StandardLogger().Init(c) }
+func EntryWithFields(ctx context.Context, fields logrus.Fields) *logrus.Entry {
+	return StandardLogger().EntryWithFields(ctx, fields)
+}
+func Infof(ctx context.Context, format string, args ...any) {
+	StandardLogger().Infof(ctx, format, args...)
+}
+func Debugf(ctx context.Context, format string, args ...any) {
+	StandardLogger().Debugf(ctx, format, args...)
+}
+func Warnf(ctx context.Context, format string, args ...any) {
+	StandardLogger().Warnf(ctx, format, args...)
+}
+func Errorf(ctx context.Context, format string, args ...any) {
+	StandardLogger().Errorf(ctx, format, args...)
+}
+func Fatalf(ctx context.Context, format string, args ...any) {
+	StandardLogger().Fatalf(ctx, format, args...)
+}
+func Panicf(ctx context.Context, format string, args ...any) {
+	StandardLogger().Panicf(ctx, format, args...)
+}
+func Info(ctx context.Context, args ...any)  { StandardLogger().Info(ctx, args...) }
+func Debug(ctx context.Context, args ...any) { StandardLogger().Debug(ctx, args...) }
+func Warn(ctx context.Context, args ...any)  { StandardLogger().Warn(ctx, args...) }
+func Error(ctx context.Context, args ...any) { StandardLogger().Error(ctx, args...) }
+func Fatal(ctx context.Context, args ...any) { StandardLogger().Fatal(ctx, args...) }
+func Panic(ctx context.Context, args ...any) { StandardLogger().Panic(ctx, args...) }
+func SetOutput(out io.Writer)                { StandardLogger().SetOutput(out) }
+func AddHook(hook logrus.Hook)               { StandardLogger().AddHook(hook) }
