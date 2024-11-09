38cc4c110ce76fabcb5bd7e473651a4c6b04c1f4
Shen <ihaiyon@gmail.com>
2024-11-09 13:12:48 +0800

    update extension metadata description

--- extension/interface.go
+++ extension/interface.go
@@ -119,9 +119,9 @@ type Metadata struct {
 	Dependencies []string `json:"dependencies,omitempty"`
 	// Description is the description of the extension
 	Description string `json:"description,omitempty"`
-	// Type is the type of the extension, e.g. plugin or module
+	// Type is the type of the extension, e.g. core, bisiness, plugin, module, etc
 	Type string `json:"type,omitempty"`
-	// Group is the belong group of the extension
+	// Group is the belong group of the extension, e.g. iam, res, flow, sys, org, rt, plug, etc
 	Group string `json:"group,omitempty"`
 }
 
